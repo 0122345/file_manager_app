45c354c41a133cfbcc020aeada38ea2639e22af6
0122345 <ntwarifiacre043@gmail.com>
2025-07-08 22:42:41 +0200

    adding splash screen & theme changing

--- fileco/lib/main.dart
+++ fileco/lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'components/file_browser/view/home.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
+
+@override
+  void initState() {
+    //super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    FlutterNativeSplash.remove();
+  }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,13 +29,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'File Manager',
       theme: ThemeData(
-         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomeScreen(), 
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-  
